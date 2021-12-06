3c43241b686a0af5afbe4f710479e582d77ae2e2
rmenai <rami.menai@outlook.com>
2021-12-06 23:11:28 +0300

    Remove tests

--- tests/app/dummy_test.py
+++
@@ -1,3 +0,0 @@
-def test_dummy():
-    """At least one test is required for a successful `0` execution code."""
-    pass
--- tests/plugins/env_vars.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import pytest
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_load_initial_conftests():
-    os.environ["DEBUG"] = "False"

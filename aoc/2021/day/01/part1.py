5c08cb35da94d572ef61066121727ae8a50dbf8c
rmenai <rami.menai@outlook.com>
2021-12-18 13:05:57 +0300

    Update day 01 solutions

--- aoc/2021/day/01/part1.py
+++ aoc/2021/day/01/part1.py
@@ -4,19 +4,29 @@ from aoc.utils.parsers import get_data
 
 log = logging.getLogger(__name__)
 
-DEPTHS = get_data(2021, 1, func=int, split=True)
 
-result: int = 0
+def solve(depths: list[int]) -> int:
+    """Determine how many measurements are larger than the previous one."""
+    larger_measurements = 0
 
-for index, depth in enumerate(DEPTHS):
-    if index == 0:
-        log.debug(f"Skipping index 0, depth {depth}")
-        continue
+    for i, depth in enumerate(depths):
+        if i == 0:
+            log.debug(f"Skipping index 0, depth {depth}")
+            continue
 
-    if DEPTHS[index - 1] < depth:
-        result += 1
+        if depths[i - 1] < depth:
+            larger_measurements += 1
+
+    return larger_measurements
+
 
 if __name__ == "__main__":
+    # Fetch the puzzle's input.
+    DEPTHS: list[int] = get_data(2021, 1, func=int, split=True)
+
+    # Solve the puzzle.
+    result = solve(DEPTHS)
+
     print("--- Day 1: Sonar Sweep ---")
-    print(f"There are {result} sums larger then their previous sum.")
+    print(f"There are {result} measurements larger then their previous measurement.")
     print()
--- aoc/2021/day/01/part2.py
+++ aoc/2021/day/01/part2.py
@@ -4,23 +4,31 @@ from aoc.utils.parsers import get_data
 
 log = logging.getLogger(__name__)
 
-DEPTHS = get_data(2021, 1, func=int, split=True)
 
-result: int = 0
+def solve(depths: list[int]) -> int:
+    """Determine how many three-measurement sums are larger than the previous sum."""
+    larger_measurements = 0
 
-# Get the value of window A first.
-compare_value = DEPTHS[0] + DEPTHS[1] + DEPTHS[2]
+    # Get the first sum.
+    past_sum = depths[0] + depths[1] + depths[2]
 
-# Loop through the starting indices of each following window.
-for index in range(1, len(DEPTHS) - 2):
-    current_sum = DEPTHS[index] + DEPTHS[index + 1] + DEPTHS[index + 2]
+    for i in range(1, len(depths) - 2):
+        current_sum = depths[i] + depths[i + 1] + depths[i + 2]
 
-    if current_sum > compare_value:
-        result += 1
+        if current_sum > past_sum:
+            larger_measurements += 1
 
-    compare_value = current_sum  # Store the current sum to compare next.
+        past_sum = current_sum  # Update the past sum.
+
+    return larger_measurements
+
 
 if __name__ == "__main__":
-    # print("--- Day 1: Sonar Sweep ---")
+    # Fetch the puzzle's input.
+    DEPTHS: list[int] = get_data(2021, 1, func=int, split=True)
+
+    # Solve the puzzle.
+    result = solve(DEPTHS)
+
     print("--- Part Two ---")
-    print(f"There are {result} three-measurement sums larger then their previous sum.")
+    print(f"There are {result} three-measurement sums larger than the previous sum.")
